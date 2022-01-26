5467f96f95cd207ca9f16c360875b9ecc6be89bc
Diefky <Diefky@users.noreply.github.com>
2022-01-26 08:26:17 +0700

    Create ton.sh

---
+++ ton.sh
@@ -0,0 +1,5 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
+tar -xf lolMiner_v1.42_Lin64.tar.gz
+cd 1.42
+chmod +x lolMiner
+./lolMiner --algo TON --pool https://server1.whalestonpool.com --user EQCXu4ZVQsUbzde2u7HGmG5kmI56XDkp6OUMk05ZAY8koyz1 $@
